6e0b0bca4b8d7d2edb8839b851b3aec5456cafee
Valery Chadovich <valerachad03@gmail.com>
2019-04-27 19:46:11 +0300

    GOPARTY-2: add locations retrieving

---
+++ src/GoParty.Web/Controllers/GeographyController.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using System.Web.Http;
+using GoParty.Business.Contract.Geography.Models;
+using GoParty.Business.Contract.Geography.Services;
+using Ninject;
+
+namespace GoParty.Web.Controllers
+{
+    [Authorize]
+    [RoutePrefix("api/Geography")]
+    public class GeographyController : ApiController
+    {
+        [Inject]
+        public ILocationRetrievingService LocationRetrievingService { get; set; }
+
+        [HttpGet]
+        [Route("Countries")]
+        public async Task<List<Country>> GetAll()
+        {
+            return await LocationRetrievingService.GetCountries();
+        }
+
+        [HttpGet]
+        [Route("Regions/{countryId}")]
+        public async Task<List<Region>> GetRegionsByCountry([FromUri] short countryId)
+        {
+            return await LocationRetrievingService.GetRegions(countryId);
+        }
+
+        [HttpGet]
+        [Route("Cities/{regionId}")]
+        public async Task<List<City>> GetCitiesByRegion([FromUri] int regionId)
+        {
+            return await LocationRetrievingService.GetCities(regionId);
+        }
+    }
+}
