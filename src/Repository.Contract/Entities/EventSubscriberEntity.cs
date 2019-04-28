e08910ab909d2c722a7dbd370bd2ebac4065c2d5
Valery Chadovich <valerachad03@gmail.com>
2019-04-28 21:30:35 +0300

    GOPARTY-8: implement events subscription logic

--- src/Repository.Contract/Entities/EventSubscriberEntity.cs
+++ src/Repository.Contract/Entities/EventSubscriberEntity.cs
@@ -1,14 +1,23 @@
 ﻿using System;
-using Repository.Contract.Entities.Contract;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace Repository.Contract.Entities
 {
-    public class EventSubscriberEntity : IEntity<Guid>
+    public class EventSubscriberEntity
     {
-        public Guid Id { get; set; }
+        [Key]
+        [Column(Order = 0)]
+        public Guid SubscriberId { get; set; }
 
+        [Key]
+        [Column(Order = 1)]
+        public Guid EventId { get; set; }
+
+        [ForeignKey(nameof(SubscriberId))]
         public virtual UserEntity Subscriber { get; set; }
 
+        [ForeignKey(nameof(EventId))]
         public virtual EventEntity Event { get; set; }
     }
 }
